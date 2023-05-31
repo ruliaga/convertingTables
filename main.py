f72df526187a08bab6645242eddf22f65a7a1f1b
Evgeny Sidorov <ruliaga@mail.ru>
2023-06-01 00:40:26 +0500

    0601_0040

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ df = controller.converting_table(df)
 print(df.info())
 controller.creating_new_file(df)
 
+# def categories(df):
+#     for i in range(0,df.shape[0]-1):
+#         if df['Технологическая операция.Наименование'].values[i].find('Сверление')!=-1:
+#             df['Технологическая операция.Наименование'].values[i] = "Сверление"
+#     return df
+# categories(df)
+# print(df[df['Технологическая операция.Наименование']])
+
