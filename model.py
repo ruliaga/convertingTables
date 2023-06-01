b6e6e630769515e294e62ffb81d8f8652a69ad3e
Evgeny Sidorov <ruliaga@mail.ru>
2023-06-02 03:20:15 +0500

    0602_0320

--- model.py
+++ model.py
@@ -5,12 +5,14 @@ import os
 
 def get_xlsx_directory (): 
     path = os.path.dirname(os.path.realpath(__file__)) #функция читает текущее расположение файла py
-    xlsx_directory = glob.glob(path + "\*.xlsx") #находит файлы xlsx и создает список из названий
+    xlsx_directory = glob.glob(path + "/*.xlsx") #находит файлы xlsx и создает список из названий
+    print(xlsx_directory)
     return xlsx_directory #возвращает этот список
 
 
+
 def xlsx_reading(xlsx_directory): #функция создает датафрейм из файла xlsx
-    df = pd.read_excel(xlsx_directory[0])
+    df = pd.read_excel(str(xlsx_directory[0]),sheet_name='TDSheet')
     return df
 
 def create_xlsx(df):
